c6d8fd1790903d1d2264e88aa0e42092e2179f29
Xideta <m.u.hermansne@gmail.com>
2018-03-15 20:49:48 +0100

    ...

--- ITU/js/button.js
+++ ITU/js/button.js
@@ -1,44 +1,19 @@
-	const butt = document.getElementsByName("Indsmid")[0];
-    const cykelInput = document.getElementsByName('cykelInput')[0];
-    const gearInput = document.getElementsByName('gearInput')[0];
-    const table = document.getElementsByTagName('table')[0];
-    const form = document.getElementsByTagName('form')[0];
-    const arrayOfBikes = [];
-    function createBikeObject(name, gear){
-        this.name = name;
-        this.gear = gear;
-        this.toString = function() {
-          'Name: '+ this.name + ', gears: ' + this.gear;
-        }
-    }
+	const butt = document.getElementById("send");
+    const nameInput = document.getElementById('nameInput');
+    const emailInput = document.getElementById('emailInput');
+    const messageInput = document.getElementById('emailInput');
+
     function disableButton(){
         butt.disabled=true;
     }
     function enableButton(){
         butt.disabled=false;
     }
-    function addBike(){
-      if(cykelInput.value!=0){
-        arrayOfBikes.push(createBikeObject(cykelInput.value,gearInput.value))
-        cykelInput.value='';
-        gearInput.value=1;
-        disableButton();
-        //let childRow = document.createElement("tr");
-        //let childCell= document.createElement("tc");
-        for(var i=0;i<arrayOfBikes.length;i++){
-          table.innerHTML+="<tr><td>" + arrayOfBikes[i].toString() + "</td></tr>";
-        }
-        console.log(arrayOfBikes.length);
+    function email(){
+        alert("Hey!");
+      if(nameInput.value!=0){
+
       }
-
-
     }
-    cykelInput.addEventListener('input', function(){
-      if(cykelInput.value.length>0){
-        enableButton();
-      } else if(cykelInput.value.length==0 ){
-        disableButton();
-      }
-    });
 
 

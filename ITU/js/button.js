21a0e72693751e612fbeca733cd1e217c1a0c3b6
Xideta <m.u.hermansen@gmail.com>
2018-03-16 01:18:00 +0100

    ...

--- ITU/js/button.js
+++ ITU/js/button.js
@@ -1,34 +1,41 @@
+    //Variables.
+    //Basically 3 locks on the submit button.
     let booleanName = false;
     let booleanEmail = false;
     let booleanMessage = false;
 
+    //I had some issues with having the links to elements out in this scope, so now it's all local
 
+    //Disables the submit button
     function disableButton(){
         const butt = document.getElementById("send");
         butt.disabled=true;
     }
+    //Enables the submit button
     function enableButton(){
         const butt = document.getElementById("send");
         butt.disabled=false;
     }
-
+    //If there's any string in the name field, set a boolean to true
     function toggleBoolName() {
         const nameInput = document.getElementById('nameInput');
         booleanName = nameInput.value.length > 0;
         checkButton();
     }
+    //If there's any email that matches a specific regex, set a boolean to true
     function toggleBoolEmail() {
         const emailInput = document.getElementById('emailInput');
         const regex = /([\w]*\+*\.*)*@([\w]*\+*).([a-zA-Z]*)/;
         booleanEmail = emailInput.value.match(regex).length > 0;
         checkButton();
     }
+    //If there's any string in message, set a boolean to true
     function toggleBoolMessage() {
         const messageInput = document.getElementById('messageInput');
         booleanMessage = messageInput.value.length > 0;
         checkButton();
     }
-
+    //If all booleans are true, enable submit button
     function checkButton() {
         if(booleanName && booleanEmail && booleanMessage){
             enableButton()
@@ -37,7 +44,8 @@
         }
 
     }
-
+    //JavaScript alert to emulate a header request.
+    //Prints an alert, and clears everything
     function email(){
         const nameInput = document.getElementById("nameInput");
         const messageInput = document.getElementById('messageInput');
@@ -45,11 +53,12 @@
         const name = nameInput.value;
         const message = messageInput.value;
         const email = emailInput.value;
+        disableButton();
         alert("Greetings "+ name +", thank you for your message, wherein you wrote:\n "+ message +"\n" +
             "We'll respond to your question by mailing "+ email +" ASAP.");
         nameInput.value='';
         emailInput.value='';
-        messageInput.value=''
+        messageInput.value='';
 
 
     }
